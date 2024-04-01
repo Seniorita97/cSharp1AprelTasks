4d71d5a74ffdc528424f6a8658caf7de73760697
Aytac <111279845+Seniorita97@users.noreply.github.com>
2024-04-01 09:50:47 +0400

    Task Finished

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Grades = new int[] {1, 4, 5, 6, 7, 8, 2 };
+            student.FilteredGrades();
+        }
+
+    }
+}
---
+++ ConsoleApp19/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Student
+    {
+        public int[] Grades;
+
+
+        public void FilteredGrades()
+        {
+            int count = 0;
+            foreach(int grade in Grades)
+            {
+                if(grade % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            int[] filteredGrades = new int[count];
+            int i = 0;
+            foreach (int grade in Grades)
+            {
+               if( grade % 2 == 0)
+                {
+                    filteredGrades[i++] = grade;
+                }
+            }
+
+            foreach (int grade in filteredGrades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+
+        public Student() 
+        {
+        }
+
+    }
+}
